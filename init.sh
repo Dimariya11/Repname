23899dcee6b49fc4e02367685438ff573ed767ac
vboxuser <vboxuser@wer.myguest.virtualbox.org>
2022-12-13 19:31:28 +0600

    Good

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/vboxuser/web/Repname/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/vboxuser/web/Repname/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
---
+++ web/hello.py
@@ -0,0 +1,9 @@
+def app(environ, start_repsonse):
+    data = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+    status = '200 OK'
+    response_headers = [
+	('Content-type', 'text/plain'),
+	('Content-length', str(len(data)))
+    ]
+    start_repsonse(status, response_headers)
+    return [bytes(data)]
